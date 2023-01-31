ee5b5a40737c787734b6314883190ae5fdda0daf
eynard27 <91985836+eynard27@users.noreply.github.com>
2023-01-30 20:57:27 -0500

    .

--- backEnd/budgetingaccounts.py
+++ backEnd/budgetingaccounts.py
@@ -1,28 +1,45 @@
+import json
+
 class BudgetAccount:
-    def __init__(self, food_budget, entertainment_budget):
-        import json
-        with open("C:/Users/William/Documents/GitHub/QHacks-2023/backEnd/USER DATA OUT.json") as o:
-            user_data_out = json.load(o)
-        self.balance = 0
-        self.food_budget = user_data_out["Outputs"]["1"]
-        self.entertainment_budget = user_data_out["Outputs"]["3"]
+    def __init__(self, balance=0, food_budget=0, entertainment_budget=0, savings_budget=0):
+        self.balance = balance
+        self.food_budget = food_budget
+        self.entertainment_budget = entertainment_budget
+        self.savings_budget = savings_budget
+        try:
+            with open("C:/Users/William/Documents/GitHub/QHacks/backEnd/USER DATA OUT.json", "r") as file:
+                data = json.load(file)
+                self.balance = data["balance"]
+                self.food_budget = data["food_budget"]
+                self.entertainment_budget = data["entertainment_budget"]
+                self.savings_budget = data["savings_budget"]
+        except:
+            self.balance = 0
+            self.food_budget = 0
+            self.entertainment_budget = 0
+            self.savings_budget = 0
 
     def check_balance(self):
         return self.balance
 
-    def track_expense(self, expense_type, amount):
-        if expense_type == "food":
-            if self.balance - amount < -self.food_budget:
-                return "Budget exceeded for food expenses"
-            self.balance -= amount
-        elif expense_type == "entertainment":
-            if self.balance - amount < -self.entertainment_budget:
-                return "Budget exceeded for entertainment expenses"
-            self.balance -= amount
+    def track_expense(self, category, amount):
+        if category == "food":
+            if self.balance - amount >= 0:
+                self.balance -= amount
+                return "Expense recorded"
+            else:
+                return "Insufficient funds"
         else:
-            return "Invalid expense type"
-my_budget = BudgetAccount(1000, 500)
+            return "Invalid category"
+
+my_budget = BudgetAccount()
 print(my_budget.check_balance())
 print(my_budget.track_expense("food", 200))
 
+with open("C:/Users/William/Documents/GitHub/QHacks/backEnd/USER DATA OUT.json", "w") as file:
+    json.dump({"balance": my_budget.balance, "food_budget": my_budget.food_budget, "entertainment_budget": my_budget.entertainment_budget, "savings_budget": my_budget.savings_budget}, file)
 
+print("balance:", my_budget.balance)
+print("Recommended food budget:", my_budget.food_budget)
+print("Recommended entertainment budget:", my_budget.entertainment_budget)
+print("Recommended savings budget:", my_budget.savings_budget)
