6d44ffaf0a09791f0f2937ed86133647d2a5da61
saithevi7 <90344380+saithevi7@users.noreply.github.com>
2023-02-02 15:55:07 -0500

    Top bar of the website

--- website-admin/src/App.js
+++ website-admin/src/App.js
@@ -1,6 +1,22 @@
-function App() {
-  <div className="app"></div>
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
 
+function App() {
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  )
 }
 
 export default App;
