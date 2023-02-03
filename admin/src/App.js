200444ebb97716db4a9f8878ef2103059cc9c606
saithevi7 <90344380+saithevi7@users.noreply.github.com>
2023-02-02 21:25:26 -0500

    beginning of Dashboard

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,9 +1,21 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import Topbar from "./scenes/global/Topbar";
 
 function App() {
-  return (
-    <div className="app">
+  const [theme, colorMode] = useMode();
 
-    </div>
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
